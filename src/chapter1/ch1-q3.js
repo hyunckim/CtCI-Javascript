a26d31b019cb9764783c2b3fb53dfa0def43d324
hyunckim <hyunkim1209@gmail.com>
2017-05-24 00:05:25 -0700

    add solution to ch1-q3

--- src/chapter1/ch1-q3.js
+++ src/chapter1/ch1-q3.js
@@ -12,4 +12,28 @@
  * @return {string[]}     Updated URL character array
  */
 export function encodeSpaces(url) {
+  if (!url || url.length === 0) {
+    return url;
+  }
+
+  let spaceCount = 0;
+
+  for (let i = 0; i < url.length; i++) {
+    if (url[i] === ' ') {
+      spaceCount++;
+    }
+  }
+
+  let newLength = url.length - 1 + 2 * spaceCount;
+
+  for (let i = url.length - 1, j = newLength; i >= 0 && j > i; i--, j--) {
+    if (url[i] === ' ') {
+      url[j] = '0';
+      url[--j] = '2';
+      url[--j] = '%';
+    } else {
+      url[j] = url[i];
+    }
+  }
+  return url;
 }
