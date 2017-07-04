0e615715dfe4a593f38aca17eb2d60aa0279bd41
hyunckim <hyunkim1209@gmail.com>
2017-07-03 23:25:54 -0700

    add solution to ch4-q03

--- src/chapter4/ch4-q03.js
+++ src/chapter4/ch4-q03.js
@@ -11,7 +11,20 @@ import { LinkedList } from './helpers';
  * Additional space: O(N)
  */
 export function listTreeByDepthOrder(tree) {
-}
+  let lists = [];
+   addToList(lists, tree.root, 0);
+   return lists;
+ }
 
-function addToList(lists, node, depth) {
+ function addToList(lists, node, depth) {
+   if (node) {
+     if (!lists[depth]) {
+       lists[depth] = new LinkedList();
+     }
+
+     lists[depth].append(node.val);
+
+     addToList(lists, node.left, depth + 1);
+     addToList(lists, node.right, depth + 1);
+   }
 }
