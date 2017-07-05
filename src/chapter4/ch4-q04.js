9d667e04466ffad7315324189b65ea2b81a8f395
hyunckim <hyunkim1209@gmail.com>
2017-07-04 23:23:00 -0700

    add solution to ch4-q04

--- src/chapter4/ch4-q04.js
+++ src/chapter4/ch4-q04.js
@@ -11,7 +11,30 @@
  * recursion, this may be O(N) in the worst case.
  */
 export function isBalanced(tree) {
-}
+  if (!tree || !tree.root) {
+      return true;
+    }
 
-function findDepth(cache, node, depth) {
+    let node = tree.root,
+      cache = {
+        min: Number.MAX_SAFE_INTEGER,
+        max: Number.MIN_SAFE_INTEGER
+      };
+
+    findDepth(cache, node, 0);
+    return cache.max - cache.min <= 1;
+  }
+
+  function findDepth(cache, node, depth) {
+    if (!node) {
+      if (depth < cache.min) {
+        cache.min = depth;
+      }
+      if (depth > cache.max) {
+        cache.max = depth;
+      }
+    } else {
+      findDepth(cache, node.left, depth + 1);
+      findDepth(cache, node.right, depth + 1);
+    }
 }
